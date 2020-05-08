17d1b08350616f6ae0d14ab81dd037d6c3340651
Neil Brown <neilbrown176@gmail.com>
2020-05-08 21:49:16 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -12,7 +12,7 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def move( board, index, character = "X")
+def move( board, index = 0, character = "X")
   board[index] = character
   return board
 end
